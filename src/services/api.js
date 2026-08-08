a27bafd74274ca8b4dc538b02b73d4128dc53f5b
Eric <wenkuan.is.me@gmail.com>
2026-08-08 13:01:32 +0800

    feat: 新增api.js

---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:8090/api";
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers:{ 'Content-Type' : 'application/json'}
+});
+
+//如果存在令牌token,新增到請求中
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if(token){
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+);
